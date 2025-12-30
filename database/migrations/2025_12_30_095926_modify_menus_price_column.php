53275fcbc6199729a1397c4b94e38fd9cabc4360
mario-005 <142988813+mario-005@users.noreply.github.com>
2025-12-30 17:01:26 +0700

    modify createmenu

---
+++ database/migrations/2025_12_30_095926_modify_menus_price_column.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('menus', function (Blueprint $table) {
+            $table->decimal('price', 12, 2)->change(); // Ubah dari (8,2) ke (12,2)
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('menus', function (Blueprint $table) {
+            $table->decimal('price', 8, 2)->change(); // Kembalikan ke (8,2)
+        });
+    }
+};
